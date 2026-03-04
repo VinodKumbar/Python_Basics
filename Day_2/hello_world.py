0302f777ff488879390ae94b62c0a52e01cba9a6
VinodKumbar <vnkumbar@gmail.com>
2026-03-04 21:56:38 +0530

    Day 2 Hello Worl File

---
+++ Day_2/hello_world.py
@@ -0,0 +1,33 @@
+# print ("Hello, World !")
+
+print ("Welcome to Python Programming")
+
+
+a = 3
+b = 5
+c = a + b
+d = "Python Programming"
+
+print ("The sum of a & b is : " ,  c)
+
+print ("********************************************************************")
+
+print(type(a))
+print(type(d))
+
+print ("********************************************************************")
+
+x , y , z = 5 , 3.3 , "Python"
+
+print ("Value of x is : " , x , " and its type is : " , type(x))
+print (type(y))
+print (type(z))
+
+print ("********************************************************************")
+
+name = input ("Enter your name : ")
+
+print ("Welcome to Python Programming, " , name)
+
+age = input ("Enter your age : ")
+print ("Your age is : " , age)
